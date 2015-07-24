16d3381f5be8007a8a55326e52e269d4abe0e102
leo <leo@bm-sms.com.tw>
2015-07-24 11:35:33 +0800

    show me the `$base`

--- test/Travis/EnvTest.php
+++ test/Travis/EnvTest.php
@@ -22,13 +22,14 @@ class EnvTest extends \PHPUnit_Framework_TestCase
 
 		$uri  = \JUri::getInstance();
 		$root = $uri::root(true);
+		$base = $uri::base(true);
 
 		var_dump(TestHelper::getValue($uri, 'uri'));
 
 		// Get site route
 		$route = Utf8String::substr($uri->getPath(), Utf8String::strlen($root));
 
-		var_dump($root, $uri->getPath(), $route);
+		var_dump($root, $base, $uri->getPath(), $route);
 
 		$this->assertEquals('rad.windwalker.io', $_SERVER['HTTP_HOST']);
 		$this->assertEquals('/flower/sakura', $_SERVER['REQUEST_URI']);
